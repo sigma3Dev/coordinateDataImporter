cfb727198c5d9bdca9cb263614ca8f6fc1cd7c6b
TWoerner94 <timwoerner94@web.de>
2018-02-28 15:03:28 +0100

    bug fixes

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ module.exports = function coordinateDataImport(pointsFile) {
   
   var reader = new FileReader();
   var blob = new Blob(pointsFile, { type: "text/plain" });
-  
+  var result = [];
+
   reader.readAsText(blob);
   reader.onloadend = function(e) {
     var parsedBlob = reader.result;
     var lines = parsedBlob.toString().split(/\r?\n/);
-    var result = lines.map(function(line) {
-        var coords = line.split(" ");
-        return ({
-          x: Number(coords[0]),
-          y: Number(coords[1]),
-          z: Number(coords[2]),
-        });
+    var linesMap = lines.map(function(line) {
+      var coords = line.split(" ");
+      return ({
+        x: Number(coords[0]),
+        y: Number(coords[1]),
+        z: Number(coords[2]),
+      });
     });
-  return result;
+    result.push(...linesMap); 
   }
+  return result;
 }
