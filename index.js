c77e351679c5c73033ce9c177b5d8c53b49c0999
TWoerner94 <timwoerner94@web.de>
2018-02-28 15:21:55 +0100

    fix git conflicts

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 "use strict";
 
 module.exports = function coordinateDataImport(pointsFile) {
-<<<<<<< HEAD
   
   var reader = new FileReader();
   var blob = new Blob(pointsFile, { type: "text/plain" });
   var result = [];
 
-=======
-  
-  var reader = new FileReader();
-  var blob = new Blob(pointsFile, { type: "text/plain" });
-  
->>>>>>> beaae9b823c655a30b0957462a6dc8787f65752b
   reader.readAsText(blob);
   reader.onloadend = function(e) {
     var parsedBlob = reader.result;
     var lines = parsedBlob.toString().split(/\r?\n/);
-<<<<<<< HEAD
-    var linesMap = lines.map(function(line) {
+    result = lines.map(function(line) {
       var coords = line.split(" ");
       return ({
         x: Number(coords[0]),
@@ -26,18 +18,6 @@ module.exports = function coordinateDataImport(pointsFile) {
         z: Number(coords[2]),
       });
     });
-    result.push(...linesMap); 
   }
-=======
-    var result = lines.map(function(line) {
-        var coords = line.split(" ");
-        return ({
-          x: Number(coords[0]),
-          y: Number(coords[1]),
-          z: Number(coords[2]),
-        });
-    });
->>>>>>> beaae9b823c655a30b0957462a6dc8787f65752b
   return result;
-  }
 }
