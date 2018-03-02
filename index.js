53c9106e820024604c8c2b62250438f0430044d6
TWoerner94 <timwoerner94@web.de>
2018-03-02 10:19:47 +0100

    add bool to decide if number should be used in calculation

--- index.js
+++ index.js
@@ -8,12 +8,19 @@ module.exports = function coordinateDataImport(pointsFile, callback) {
   reader.onloadend = function(e) {
     var parsedBlob = reader.result;
     var lines = parsedBlob.toString().split(/\r?\n/);
-    var lineMap = lines.map(function(line) {
+    var lineMap = lines.map(function(line, index) {
       var coords = line.split(" ");
+      var xId = 'x' + index;
+      var yId = 'y' + index;
+      var zId = 'z' + index;
       return ({
         x: Number(coords[0]),
         y: Number(coords[1]),
         z: Number(coords[2]),
+        xId: true,
+        yId: true,
+        zId: true
+        
       });
     });
     callback(lineMap);
