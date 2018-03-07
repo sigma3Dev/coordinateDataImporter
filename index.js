ec4aa4c454fffafa314e924568c456a17d79e170
TWoerner94 <timwoerner94@web.de>
2018-03-07 13:26:29 +0100

    now two functions: startCoordinateDataImport and targetCoordinateDataImport

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 "use strict";
 
-module.exports = function coordinateDataImport(pointsFile, callback) { 
-  var reader = new FileReader();
-  var blob = new Blob(pointsFile, { type: "text/plain" });
+module.exports = {
+  startCoordinateDataImport: function(pointsFile, callback) { 
+    var reader = new FileReader();
+    var blob = new Blob(pointsFile, { type: "text/plain" });
 
-  reader.readAsText(blob);
-  reader.onloadend = function(e) {
-    var parsedBlob = reader.result;
-    var lines = parsedBlob.toString().split(/\r?\n/);
-    var lineMap = lines.map(function(line, index) {
-      var coords = line.split(" ");
-      var obj = {        
-        x: Number(coords[0]),
-        y: Number(coords[1]),
-        z: Number(coords[2])
-      };
-      obj['x' + index] = true;
-      obj['y' + index] = true;
-      obj['z' + index] = true;
-      return (obj);
-    });
-    callback(lineMap);
+    reader.readAsText(blob);
+    reader.onloadend = function(e) {
+      var parsedBlob = reader.result;
+      var lines = parsedBlob.toString().split(/\r?\n/);
+      var lineMap = lines.map(function(line, index) {
+        var coords = line.split(" ");
+        var obj = {        
+          x: Number(coords[0]),
+          y: Number(coords[1]),
+          z: Number(coords[2])
+        };
+        return (obj);
+      });
+      callback(lineMap);
+    }
+  },
+
+  targetCoordinateDataImport: function(pointsFile, callback) { 
+    var reader = new FileReader();
+    var blob = new Blob(pointsFile, { type: "text/plain" });
+
+    reader.readAsText(blob);
+    reader.onloadend = function(e) {
+      var parsedBlob = reader.result;
+      var lines = parsedBlob.toString().split(/\r?\n/);
+      var lineMap = lines.map(function(line, index) {
+        var coords = line.split(" ");
+        var obj = {        
+          x: Number(coords[0]),
+          y: Number(coords[1]),
+          z: Number(coords[2])
+        };
+        obj.useX = true;
+        obj.useY = true;
+        obj.useZ = true;
+        return (obj);
+      });
+      callback(lineMap);
+    }
   }
 }
