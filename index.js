81f48bcb712f85f7979c3bb261bc612efcd5d97e
Tim <timwoerner94@web.de>
2018-04-26 13:13:23 +0200

    actually go back to ES5

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+'use strict';
+
 module.exports = {
-  startCoordinateDataImport(pointsFile, callback) {
+  startCoordinateDataImport: function startCoordinateDataImport(pointsFile, callback) {
     var reader = new FileReader();
     var blob = new Blob(pointsFile, { type: 'text/plain' });
 
@@ -12,17 +14,18 @@ module.exports = {
         var obj = {
           x: Number(coords[0]),
           y: Number(coords[1]),
-          z: Number(coords[2]),
+          z: Number(coords[2])
         };
         if (!Number.isNaN(obj.x) && !Number.isNaN(obj.y) && !Number.isNaN(obj.z)) {
           return obj;
         }
       });
-      callback(lineMap.filter(function (line) { return line !== undefined }));
+      callback(lineMap.filter(function (line) {
+        return line !== undefined;
+      }));
     };
   },
-
-  targetCoordinateDataImport(pointsFile, callback) {
+  targetCoordinateDataImport: function targetCoordinateDataImport(pointsFile, callback) {
     var reader = new FileReader();
     var blob = new Blob(pointsFile, { type: 'text/plain' });
 
@@ -38,17 +41,18 @@ module.exports = {
           z: Number(coords[2]),
           useX: true,
           useY: true,
-          useZ: true,
+          useZ: true
         };
         if (!Number.isNaN(obj.x) && !Number.isNaN(obj.y) && !Number.isNaN(obj.z)) {
           return obj;
         }
       });
-      callback(lineMap.filter(function (line) { return line !== undefined }));
+      callback(lineMap.filter(function (line) {
+        return line !== undefined;
+      }));
     };
   },
-
-  bundleAdjustmentDataImport(pointsFile, callback) {
+  bundleAdjustmentDataImport: function bundleAdjustmentDataImport(pointsFile, callback) {
     var reader = new FileReader();
     var blob = new Blob(pointsFile, { type: 'text/plain' });
 
@@ -64,13 +68,15 @@ module.exports = {
           x: Number(coords[2]),
           y: Number(coords[3]),
           z: Number(coords[4]),
-          stdev: Number(coords[5]),
+          stdev: Number(coords[5])
         };
         if (!Number.isNaN(obj.stationId) && !Number.isNaN(obj.geometryId) && !Number.isNaN(obj.x) && !Number.isNaN(obj.y) && !Number.isNaN(obj.z) && !Number.isNaN(obj.stdev)) {
           return obj;
         }
       });
-      callback(lineMap.filter(function (line) { return line !== undefined }));
+      callback(lineMap.filter(function (line) {
+        return line !== undefined;
+      }));
     };
-  },
+  }
 };
